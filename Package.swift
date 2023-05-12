16f7232b155a08b04f86e6088f5515220242b3e6
Jiang, Chao <chao.jiang@adidas.com>
2023-05-12 16:46:11 +0800

    1.0.7 for build on xcode14.3+

--- Package.swift
+++ Package.swift
@@ -25,23 +25,23 @@ let package = Package(
         ),
         .binaryTarget(
             name: "account_login_sdk_noui_core",
-            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.6/account_login_sdk_noui_core.xcframework.zip",
-            checksum: "9057de76e820e27af59c4560eb4bc8174014bcba1509735edd66d358767278f7"
+            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.7/account_login_sdk_noui_core.xcframework.zip",
+            checksum: "ea322747a26b87065ebcd000214fbd8dadb000b7b0c6b095b810beed1db0f8c8"
         ),
         .binaryTarget(
             name: "EAccountApiSDK",
-            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.6/EAccountApiSDK.xcframework.zip",
-            checksum: "15fc53d86c58cdb9353a39c96d3bf368a079c5985a89207d248ca59a903f01e7"
+            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.7/EAccountApiSDK.xcframework.zip",
+            checksum: "a1d74791e3fa98c23ec481d9159f034a488af25dab8aa0f2694568a7ffca7e4a"
         ),
         .binaryTarget(
             name: "TYRZUISDK",
-            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.6/TYRZUISDK.xcframework.zip",
-            checksum: "f8f91f31802ecca3ec26efc4ee558d1d76132744b2fe5c990c465b514351d1e7"
+            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.7/TYRZUISDK.xcframework.zip",
+            checksum: "7f6a3f33ff1bad5f8cc5adfc0fad1498f5dcc03d86048f00ee7e172f054de742"
         ),
         .binaryTarget(
             name: "OneLoginSDK",
-            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.6/OneLoginSDK.xcframework.zip",
-            checksum: "0753b95b07d731b1dc1b39892a831fe8c75bed46f7bb7863fb5b9905c29330fb"
+            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.7/OneLoginSDK.xcframework.zip",
+            checksum: "eec8b789e1425147959c2d2f88fd9918e484074a1e82b19b10740e5d46b3917b"
         )
     ]
 )
