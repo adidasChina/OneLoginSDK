a512135b512c40a437160ca228886a52ef01b430
Jiang, Chao <chao.jiang@adidas.com>
2023-05-17 09:37:05 +0800

    use xcframework

--- Package.swift
+++ Package.swift
@@ -25,23 +25,23 @@ let package = Package(
         ),
         .binaryTarget(
             name: "account_login_sdk_noui_core",
-            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.8/account_login_sdk_noui_core.xcframework.zip",
-            checksum: "3ac333eb98763f7150ebe43ff1c88a2c2ee5066d9eb0ff1e66b15020ed6be9f6"
+            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.9/account_login_sdk_noui_core.xcframework.zip",
+            checksum: "9a4c5b893e57ca3bb19472c0fa6c1b3c09e73260d61bc14c37ae1b61d1e7c919"
         ),
         .binaryTarget(
             name: "EAccountApiSDK",
-            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.8/EAccountApiSDK.xcframework.zip",
-            checksum: "8217af0b8ecddd3a90e37856c4ef716df1005dc39e4b7fa8b9ee10628f15baf3"
+            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.9/EAccountApiSDK.xcframework.zip",
+            checksum: "14a886b01e2f8a343f76072f339ff0b1f0537aefe85f8707e2b783026845c808"
         ),
         .binaryTarget(
             name: "TYRZUISDK",
-            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.8/TYRZUISDK.xcframework.zip",
-            checksum: "8edf7c6b57bc68164dda131e3bade1e57c8a17e2792505ed33bf210c939a16ea"
+            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.9/TYRZUISDK.xcframework.zip",
+            checksum: "16ba5a561fad52f9bfdaa4268f684278fc481483f1b783d823eb77d6ba2a5650"
         ),
         .binaryTarget(
             name: "OneLoginSDK",
-            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.8/OneLoginSDK.xcframework.zip",
-            checksum: "5ee770b1e1733bbcc6e04e17b2de79eb6e4edbfe6635d189c2b619f059d36c67"
+            url: "https://github.com/adidasjc/OneLoginSDK/releases/download/1.0.9/OneLoginSDK.xcframework.zip",
+            checksum: "b913c48f3955f72f422b3f787181bb5a7f3747e198d75e376f482f0c4f4aa14d"
         )
     ]
 )
